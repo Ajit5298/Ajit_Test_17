a2346129451fe3df74dd731114e11235eb663c3b
Ajit5298 <vikeekamble01@gmail.com>
2023-03-17 12:51:07 +0530

    one output getting out of 4

---
+++ dsatest.js
@@ -0,0 +1,31 @@
+// Q-1
+// var array = [34,45,65,23,5,676,32,34];
+// var target = 66;
+// for(var i = 0; i < array.length - 1; i++ )
+// {
+//     for(var j = i + 1 ; j < array.length ; j++)
+//     { 
+//         if(array[i] + array[j]  === target)
+//         {
+//             console.log(array[i],array[j])
+//         }
+//     }
+// }
+
+
+// var array=[3,5,79,45,0,-67,23,77,19,78,12]
+// var num=19
+
+// for(var i = 0; i < array.length ; i++ )
+// {
+//     for(var j =array["i"]  ; j < array.length ; j++)
+//     { 
+//         if(array["i"]  === num)
+//         {
+//             console.log("number is prsent")
+//         }
+//         else{
+//             console.log("number is not prsent")
+//         }
+//     }
+// }
